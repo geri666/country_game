9db93fb4ae51f329caf05862c60c1a5363af7d75
g.szijarto.inf19 <g.szijarto.inf19@stud.bbbaden.ch>
2022-03-24 10:03:13 +0100

    added api access + displays random flag smiley emoji

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
+var countries;
+var currentCountry;
+
 async function loadCountries() {
   let response = await fetch("https://restcountries.com/v3.1/all");
   let data = await response.json();
   return data;
 }
 
-$(document).ready(function () {
-    let countries = loadCountries();
-    console.log(countries);
+$(document).ready(async function () {
+  countries = await loadCountries();
+  currentCountry = countries[getRandomInt(countries.length)];
+
+  document.getElementById("flagContainer").src = currentCountry.flags.png;
 });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
