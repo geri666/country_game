007c3e3b7f71229aaa7a71b0344d729b73c93928
g.szijarto.inf19 <g.szijarto.inf19@stud.bbbaden.ch>
2022-03-24 11:12:14 +0100

    added guessing stuff

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 var countries;
 var currentCountry;
+var guessesLeft;
 
 async function loadCountries() {
   let response = await fetch("https://restcountries.com/v3.1/all");
@@ -8,12 +9,36 @@ async function loadCountries() {
 }
 
 $(document).ready(async function () {
-  countries = await loadCountries();
-  currentCountry = countries[getRandomInt(countries.length)];
+  startGame();
+});
+
+async function startGame() {
+  guessesLeft = 5;
+  document.getElementById("userInput").value = "";
+
+  countries = await loadCountries(); // doesn't need to be called on new game tho ... 
+  currentCountry = countries[getRandomInt(countries.length)]; // loads new country
 
   document.getElementById("flagContainer").src = currentCountry.flags.png;
-});
+  console.log(currentCountry.name.common);
+}
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+function guess() {
+  let userGuess = document.querySelector("#userInput").value.toLowerCase();
+
+  if (userGuess == currentCountry.name.common.toLowerCase()) {
+    countryGuessed();
+  } else {
+    guessesLeft--;
+  }
+  document.getElementById("userInput").value = "";
+}
+
+function countryGuessed(){
+    console.log("congratulations! you guessed the country with " + guessesLeft + " guesses left.");
+    startGame();
 }
